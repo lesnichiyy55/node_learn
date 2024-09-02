53719d93851d494ab7d16c97fc7f5ce7a29949f9
Alex <lesnihiyy@gmail.com>
2024-09-02 15:10:23 +0300

    добавил функ-ю createArrs, для формирования двумерного массива

---
+++ 5-multithreading/createArrs.js
@@ -0,0 +1,21 @@
+/**
+ * createArrs - создает массив из 300 000 чисел и возвращает
+ * двумерный массив из 16 элементов при помощи splitArr, принимающая
+ * исходных массив и нужное количество элементов 
+ */
+
+function createArrs() {
+    let arr = Array.from(Array(300001).keys()).splice(1);
+    const count = arr.length / 16;
+    splitArr(arr, count);
+}
+
+function splitArr(arr, count) {
+    const arrs = [];
+    while (arr.length) {
+        arrs.push(arr.splice(0, count));
+    };
+    return arrs;
+};
+
+module.exports = {createArrs, splitArr}
