c8f3f11396e8dbf5160d44560e28087d12a02175
Alex <lesnihiyy@gmail.com>
2024-09-04 13:47:13 +0300

    начинаю писать api.service.js

--- 8-weather-cli/weather.js
+++ 8-weather-cli/weather.js
@@ -3,7 +3,14 @@ import {printHelp, printSuccess, printError} from "./services/log-service.js"
 import {getArgs} from "./helpers/args.js"
 import { saveKeyValue } from "./storage/storage.service.js";
 
+//fd01cf0b37669673e4ec99090d9ffcde
+
 const saveToken = async (token) => {
+
+    if(!token.length) {
+        printError("Не передан токен")
+        return
+    }
      try {
          await saveKeyValue("token", token);
          printSuccess('Токен сохранен');
